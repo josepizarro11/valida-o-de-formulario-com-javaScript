6c19a75eb2650fa21c669ee6541c13079159fbe8
m3psud <pizarromorales11@gmail.com>
2022-11-19 23:03:40 -0300

    validacao de formulario

---
+++ index.js
@@ -0,0 +1,77 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const assunto= document.getElementById('assunto')
+const mensagem = document.getElementById('mensagem')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkInputs()
+})
+
+function checkInputs() {
+
+    const usernameValue = username.value.trim()
+    const emailValue = email.value.trim()
+    const assuntoValue = assunto.value.trim()
+    const mensagemValue = mensagem.value.trim()
+
+    if(usernameValue === '') {
+    
+        setErrorFor(username, 'Preencha esse campo')
+    } else {
+        
+        setSuccessFor(username)
+    }
+
+    if(emailValue === '') {
+       
+        setErrorFor(email, 'Preencha esse campo')
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email inválido')
+    } else {
+       
+        setSuccessFor(email)
+    }
+
+    if(assuntoValue === '') {
+       
+        setErrorFor(assunto, 'Preencha esse campo')
+
+    } else if(assuntoValue.length < 8) { 
+        setErrorFor(assunto, 'Assunto deve ter mais que 8 caracteres')
+    } else {
+    
+        setSuccessFor(assunto)
+    }
+
+    if(mensagemValue === '') {
+       
+        setErrorFor(mensagem, 'Preencha esse campo')
+
+      
+        setSuccessFor(mensagem)
+    }
+
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+
+    small.innerText = message
+
+    formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success'
+}
+
+function isEmail(email) {
+    return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
+}
+
